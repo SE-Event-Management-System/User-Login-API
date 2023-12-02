77c0861f4f9661097fe3697ccd9fc25561ba1060
Rutvij Dave <rutvijdave@Rutvijs-MacBook-Pro.local>
2023-12-02 15:21:44 -0500

    User Model and JWT changed

--- src/routes/login.service.js
+++ src/routes/login.service.js
@@ -46,7 +46,8 @@ async function loginService(req, res, next){
             const token = jwt.sign({
                             user: userList[0].firstName,
                             email: userList[0].email,
-                            userId: userList[0]._id
+                            userId: userList[0]._id,
+                            isAdmin: userList[0].isAdmin
                         },
                         jwtSecretKey,
                         {
